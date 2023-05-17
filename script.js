2da798d80f5c6d854582711a3165af9c63cff464
anibloe <ashley.nibloe@gmail.com>
2023-05-17 22:20:20 +0100

    Refactoring.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (() => {
-	const fetchExternalData = () => {
+	const fetchJSON = (() => {
 		// prettier-ignore
 		return Promise.all([
       fetch("./IITC-pogo_new.json"),
@@ -8,45 +8,40 @@
       fetch("./IITC-pogo_desktop.json"),
       fetch("./IITC-pogo_old.json")
     ])
-    .then(results => {
-      return Promise.all(results.map(result => result.json()));
-    });
-	};
-
-	fetchExternalData()
+    .then(jsonObjs => {
+      return Promise.all(jsonObjs.map(jsonObj => jsonObj.json()));
+    })
 		.then((arrJsonObjs) => {
 			const used = [];
+			const nObj = {};
 
-			const newObj = {
-				gyms: {},
-				pokestops: {},
-				notpogo: {},
-				ignoredCellsExtraGyms: {},
-				ignoredCellsMissingGyms: {},
-			};
+			arrJsonObjs.forEach((oObj) => {
+				const oCats = Object.keys(oObj);
 
-			arrJsonObjs.forEach((jsonObj) => {
-				const cats = Object.keys(jsonObj);
+				oCats.forEach((oCat) => {
+					const oItemIds = Object.keys(oObj[oCat]);
 
-				cats.forEach((key) => {
-					const itemIds = Object.keys(jsonObj[key]);
-
-					itemIds.forEach((itemId) => {
-						const isDone = used.includes(itemId);
+					oItemIds.forEach((oItemId) => {
+						const isDone = used.includes(oItemId);
 
 						if (!!isDone) return;
 
-						used.push(itemId);
+						used.push(oItemId);
 
-						newObj[key][itemId] = jsonObj[key][itemId];
+						if (!nObj[oCat]) nObj[oCat] = {};
+
+						nObj[oCat][oItemId] = oObj[oCat][oItemId];
 					});
 				});
 			});
 
 			const elLink = document.createElement("a");
-			elLink.href = window.URL.createObjectURL(new Blob([JSON.stringify(newObj)], { type: "text/plain" }));
+			elLink.href = window.URL.createObjectURL(new Blob([JSON.stringify(nObj)], { type: "text/plain" }));
 			elLink.download = "IITC-pogo__output.json";
 			elLink.click();
+
+			console.log(nObj);
 		})
 		.catch(console.error);
+	})();
 })();
